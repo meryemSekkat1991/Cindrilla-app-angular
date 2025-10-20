610fd4c14a2a411184d1966998285d1a9753d20e
Meryem Sekkat <meryemsekkat1991@gmail.com>
2025-10-20 11:30:25 +0100

    config tailwind

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      "plugins": {
+        "@tailwindcss/postcss": {}
+      }
+    },
+    // Add other plugins here, e.g., 'tailwindcss/postcss': {}
+  }
+};
