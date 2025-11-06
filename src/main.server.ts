0a24998bcc3f5d69f3f2f8bf551bf2cf6a787893
Meryem Sekkat <meryemsekkat1991@gmail.com>
2025-11-06 18:37:44 +0100

    c-6: production fix

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,22 @@
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-import { config } from './app/app.config.server';
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr'
+import { getContext } from '@netlify/angular-runtime/context.mjs'
 
-const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, config, context);
+const angularAppEngine = new AngularAppEngine()
 
-export default bootstrap;
+export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
+  const context = getContext()
+
+  // Example API endpoints (optional)
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  const result = await angularAppEngine.handle(request, context)
+  return result || new Response('Not found', { status: 404 })
+}
+
+/**
+ * The request handler used by the Angular CLI (dev-server and during build).
+ */
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler)
